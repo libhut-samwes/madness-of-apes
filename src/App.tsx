35af278ab8c7af966832fb2786cbf1b3b719370c
Libhut Samwes <libhut-samwes@protonmail.com>
2022-02-17 14:45:34 +0800

    add visor to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import { urbitVisor } from '@dcspark/uv-core';
 import Header from './components/Header';
 import './App.css'
 
-
 function App() {
+	const [ship, setShip] = useState('');
+	useEffect(() => {
+		urbitVisor.require(['shipName'], setData);
+	}, []);
+	function setData() {
+		urbitVisor.getShip().then((res) => setShip(res.response));
+	}
+
+	
 	return (
 		<div className="App">
 			<Header />
